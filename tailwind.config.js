425664f724553629064d9f21eb5310ef3b8997cc
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2021-08-17 12:36:18 +0800

    :wrench: change tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
+  purge: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx', './src/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
