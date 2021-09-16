3b6d8b000d91deec88df6384e40841e0a776f97b
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2021-09-16 13:33:13 +0800

    :sparkles: update deeplink protocol

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
 
   data() {
-    return {}
+    return {};
   },
 
   components: {},
@@ -15,56 +15,63 @@ export default {
   created() {},
 
   mounted() {
-    this.wakeUpQuickapp()
+    this.wakeUpQuickapp();
   },
 
   methods: {
     isAndroidSystem() {
-      const ua = window.navigator.userAgent.toLowerCase()
-      return ua.indexOf('android') > -1 || ua.indexOf('adr') > -1
+      const ua = window.navigator.userAgent.toLowerCase();
+      return ua.indexOf("android") > -1 || ua.indexOf("adr") > -1;
     },
 
     isWechatBrowser() {
-      const ua = window.navigator.userAgent.toLowerCase()
-      return ua.includes('wechat') || ua.includes('wexin')
+      const ua = window.navigator.userAgent.toLowerCase();
+      return ua.includes("wechat") || ua.includes("wexin");
     },
 
     wakeUpQuickappByDeeplink() {
-      const protocol = this.isWechatBrowser() ? 'https://hapjs.org/' : 'https://hapjs.org/' // 'hap://'
-      const aNodeDom = document.createElement('a')
-      aNodeDom.setAttribute('href', `${protocol}app/com.graceful.sentences/pages/Main`)
-      aNodeDom.setAttribute('target', '_self')
-      aNodeDom.setAttribute('id', 'open-in-quickapp')
-      if (document.getElementById('open-in-quickapp')) {
-        document.body.removeChild(document.getElementById('open-in-quickapp'))
+      const protocol = this.isWechatBrowser()
+        ? "http://hapjs.org/"
+        : "http://hapjs.org/"; // 'hap://'
+      const aNodeDom = document.createElement("a");
+      aNodeDom.setAttribute(
+        "href",
+        `${protocol}app/com.graceful.sentences/pages/Main`
+      );
+      aNodeDom.setAttribute("target", "_self");
+      aNodeDom.setAttribute("id", "open-in-quickapp");
+      if (document.getElementById("open-in-quickapp")) {
+        document.body.removeChild(document.getElementById("open-in-quickapp"));
       }
-      document.body.appendChild(aNodeDom)
+      document.body.appendChild(aNodeDom);
       setTimeout(() => {
-        aNodeDom.click()
-      }, 100)
+        aNodeDom.click();
+      }, 100);
     },
 
     wakeUpQuickappByUrl() {
-      const random = Math.random()
-      fetch(`http://thefatherofsalmon.com/?i=com.graceful.sentences&random=${random}`)
-        .then(res => res.json())
-        .then(res => console.log(res))
+      const random = Math.random();
+      fetch(
+        `http://thefatherofsalmon.com/?i=com.graceful.sentences&random=${random}`
+      )
+        .then((res) => res.json())
+        .then((res) => console.log(res));
     },
 
     wakeUpQuickapp() {
-      if (!this.isAndroidSystem()) return
+      if (!this.isAndroidSystem()) return;
 
-      const ua = window.navigator.userAgent.toLowerCase()
-      const browserArr = ['ucbrowser', 'mqqbrowser', 'quark', 'baidu']
-      const isSupportUrl = browserArr.some(elem => {
-        return ua.includes(elem)
-      })
+      const ua = window.navigator.userAgent.toLowerCase();
+      const browserArr = ["ucbrowser", "mqqbrowser", "quark", "baidu"];
+      const isSupportUrl = browserArr.some((elem) => {
+        return ua.includes(elem);
+      });
       if (isSupportUrl) {
-        this.wakeUpQuickappByUrl()
+        this.wakeUpQuickappByUrl();
       } else {
-        this.wakeUpQuickappByDeeplink()
+        this.wakeUpQuickappByDeeplink();
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
